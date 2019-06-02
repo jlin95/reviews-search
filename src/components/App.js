bf1bb72fe0fa415845b4f5025d67337ec298e67d
Sam Lee <leeway95@gmail.com>
2019-06-02 15:53:09 +0800

    Clean up README file.

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <div>HelloHello</div>
+      </header>
+    </div>
+  )
+}
+
+export default App
